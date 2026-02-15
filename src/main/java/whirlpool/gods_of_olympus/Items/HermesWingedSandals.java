3b562e463bf99c6689b265df32ec0032ed60a3bf
Whirlpoool <leobulatewicz@gmail.com>
2026-02-15 14:13:22 -0500

    Hermes sandals work rn no drawbacks though

---
+++ src/main/java/whirlpool/gods_of_olympus/Items/HermesWingedSandals.java
@@ -0,0 +1,26 @@
+package whirlpool.gods_of_olympus.Items;
+
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.neoforged.neoforge.common.extensions.IItemExtension;
+import org.jspecify.annotations.Nullable;
+
+public class HermesWingedSandals extends Item implements IItemExtension {
+    public static final int FLIGHT_TIME = 1200; // 60 seconds in ticks (20 ticks per second)
+
+    public HermesWingedSandals(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public void inventoryTick(ItemStack stack, ServerLevel level, Entity entity, @Nullable EquipmentSlot slot) {
+        if(entity instanceof Player player && player.getItemBySlot(EquipmentSlot.FEET).getItem() instanceof HermesWingedSandals hermesWingedSandals) {
+            player.getAbilities().mayfly = true;
+            player.onUpdateAbilities();
+        }
+    }
+}
--- src/main/java/whirlpool/gods_of_olympus/Registry/ModItems.java
+++ src/main/java/whirlpool/gods_of_olympus/Registry/ModItems.java
@@ -1,144 +1,92 @@
 package whirlpool.gods_of_olympus.Registry;
 
 import net.minecraft.core.component.DataComponents;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.component.Weapon;
+import net.minecraft.world.item.equipment.ArmorMaterial;
+import net.minecraft.world.item.equipment.Equippable;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import whirlpool.gods_of_olympus.Gods_of_olympus;
+import whirlpool.gods_of_olympus.Items.HermesWingedSandals;
 import whirlpool.gods_of_olympus.Items.PoseidonsTrident;
 import whirlpool.gods_of_olympus.Renderer.ModRarities;
 
-/**
- * Registry class for all items in the Gods of Olympus mod.
- * This class uses DeferredRegister to register items with the NeoForge
- * registry.
- */
 public class ModItems {
-    // DeferredRegister for Items. Acts as a holding list for all items before they are registered to the game.
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(Gods_of_olympus.MODID);
 
-    /**
-     * Poseidon's Trident.
-     * 
-     * A custom legendary weapon implementation {@link PoseidonsTrident} that
-     * defines specific behaviors.
-     * 
-     * Configured with:
-     * Custom Rarity: {@link ModRarities#POSEIDON}
-     * Durability: 1000 
-     * Custom Attributes (e.g., Reach, Damage)
-     * Tool properties (mining speed, etc.)
-     * Weapon properties (attack damage, etc.)
-     * 
-     */
     public static final DeferredItem<Item> POSEIDONS_TRIDENT = ITEMS.registerItem("poseidons_trident",
-        properties -> new PoseidonsTrident(
-                properties
-                        .rarity(ModRarities.POSEIDON)
-                        .durability(1000)
-                        .attributes(PoseidonsTrident.createAttributes())
-                        .component(DataComponents.TOOL, PoseidonsTrident.createToolProperties())
-                        .enchantable(5)
-                        .component(DataComponents.WEAPON, new Weapon(1))
-        ));
+            properties -> new PoseidonsTrident(
+                    properties
+                            .rarity(ModRarities.POSEIDON)
+                            .durability(1000)
+                            .attributes(PoseidonsTrident.createAttributes())
+                            .component(DataComponents.TOOL, PoseidonsTrident.createToolProperties())
+                            .enchantable(5)
+                            .component(DataComponents.WEAPON, new Weapon(1))
+            )); //Create a new PoseidonsTrident Item called "poseidons_trident" and set its rarity to EPIC
 
-    /**
-     * Zeus's Master Bolt.
-     * Currently registered as a generic Item with EPIC rarity.
-     */
     public static final DeferredItem<Item> ZEUS_MASTER_BOLT = ITEMS.registerItem("zeus_master_bolt",
-        properties -> new Item(
-                properties
-                        .rarity(Rarity.EPIC)
-                        .durability(1000) // SUBJECT TO CHANGE
-        ));
+            properties -> new Item(
+                    properties
+                            .rarity(Rarity.EPIC)
+                            .durability(1000) //SUBJECT TO CHANGE
+            ));
 
-    /**
-     * Hades's Helm of Darkness.
-     * Currently registered as a generic Item with EPIC rarity.
-     */
     public static final DeferredItem<Item> HADES_HELM_OF_DARKNESS = ITEMS.registerItem("hades_helm_of_darkness",
-        properties -> new Item(
-                properties
-                        .rarity(Rarity.EPIC)
-                        .durability(1000) // SUBJECT TO CHANGE
-        ));
-
-    /**
-     * Aegis Shield of Athena.
-     * Currently registered as a generic Item with EPIC rarity.
-     */
-    public static final DeferredItem<Item> AEGIS_SHIELD_OF_ATHENA = ITEMS.registerItem("aegis_shield_of_athena",
-        properties -> new Item(
+            properties -> new Item(
                     properties
                             .rarity(Rarity.EPIC)
-                            .durability(1000) // SUBJECT TO CHANGE
-        ));
+                            .durability(1000) //SUBJECT TO CHANGE
+            ));
 
-    /**
-     * Apollo's Bow of Light.
-     * Currently registered as a generic Item with EPIC rarity.
-     */
+    public static final DeferredItem<Item> AEGIS_SHEILD_OF_ATHENA = ITEMS.registerItem("aegis_sheild_of_athena",
+            properties -> new Item(
+                    properties
+                            .rarity(Rarity.EPIC)
+                            .durability(1000) //SUBJECT TO CHANGE
+            ));
+
     public static final DeferredItem<Item> APOLLOS_BOW_OF_LIGHT = ITEMS.registerItem("apollos_bow_of_light",
-        properties -> new Item(
+            properties -> new Item(
                     properties
                             .rarity(Rarity.EPIC)
-                            .durability(1000) // SUBJECT TO CHANGE
-        ));
+                            .durability(1000) //SUBJECT TO CHANGE
+            ));
 
-    /**
-     * Artemis's Bow of the Hunt.
-     * Currently registered as a generic Item with EPIC rarity.
-     */
     public static final DeferredItem<Item> ARTEMIS_BOW_OF_THE_HUNT = ITEMS.registerItem("artemis_bow_of_the_hunt",
-        properties -> new Item(
+            properties -> new Item(
                     properties
                             .rarity(Rarity.EPIC)
-                            .durability(1000) // SUBJECT TO CHANGE
-        ));
+                            .durability(1000) //SUBJECT TO CHANGE
+            ));
 
-    /**
-     * Ares's Spear of War.
-     * Currently registered as a generic Item with EPIC rarity.
-     */
     public static final DeferredItem<Item> ARES_SPEAR_OF_WAR = ITEMS.registerItem("ares_spear_of_war",
-        properties -> new Item(
+            properties -> new Item(
                     properties
                             .rarity(Rarity.EPIC)
-                            .durability(1000) // SUBJECT TO CHANGE
-        ));
+                            .durability(1000) //SUBJECT TO CHANGE
+            ));
 
-    /**
-     * Hermes's Winged Sandals.
-     * Currently registered as a generic Item with EPIC rarity.
-     */
     public static final DeferredItem<Item> HERMES_WINGED_SANDALS = ITEMS.registerItem("hermes_winged_sandals",
-        properties -> new Item(
+            properties -> new HermesWingedSandals(
                     properties
+                            .component(DataComponents.EQUIPPABLE, Equippable.builder(EquipmentSlot.FEET).build())
                             .rarity(Rarity.EPIC)
-                            .durability(1000) // SUBJECT TO CHANGE
-        ));
+                            .enchantable(15)
+                            .durability(481) //SUBJECT TO CHANGE
+            ));
 
-    /**
-     * Hephaestus's Hammer of Fire.
-     * Currently registered as a generic Item with EPIC rarity.
-     */
     public static final DeferredItem<Item> HEPHAESTUS_HAMMER_OF_FIRE = ITEMS.registerItem("hephaestus_hammer_of_fire",
-        properties -> new Item(
+            properties -> new Item(
                     properties
                             .rarity(Rarity.EPIC)
-                            .durability(1000) // SUBJECT TO CHANGE
-        ));
+                            .durability(1000) //SUBJECT TO CHANGE
+            ));
 
-    /**
-     * Registers the DeferredRegister to the event bus.
-     * This method must be called in the main mod class constructor.
-     *
-     * @param eventBus The mod event bus
-     */
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
     }
